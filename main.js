6d668c0bdebc499b070aba0e8976d9a23a44930e
Osemwengie Courage <courageosemwengie@gmail.com>
2019-02-11 16:31:35 +0100

    new post

--- main.js
+++ main.js
@@ -4,12 +4,7 @@ document.write("<p>This is what <strong>matters</strong></p>")
 document.getElementById("h1").innerHTML = "you are welcome  in the name of the lord"
 document.getElementById("firstParagraph").innerHTML = "This is a <strong>special</strong> paragraph";
 console.log("Testing...")
-
-var contact = {
-    firstname: "john",
-    lastname: okafor,
-    age: 56
-
-
-}
-contact.age
+var p = 91;
+var l = 42;
+document.write(<br />)
+document.write(p/l)
